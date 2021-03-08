0b2aea16557e1962f131861dd1f7bd064e2a81f0
prateek dubey <prateek@prateeks-MacBook-Air.local>
2021-03-08 21:48:42 +0530

    portfolio section removed

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -26,7 +26,6 @@ const Header = (props) => {
                <li><a className="smoothscroll" href="#about">About</a></li>
                <li><a className="smoothscroll" href="#resume">Resume</a></li>
                <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-               <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
 
